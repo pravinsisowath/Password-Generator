c6393403508940ea90357e42bbc144318c5659fe
Pravin Sisowath <pravinsisowath@gmail.com>
2020-04-23 15:10:53 -0700

    created password generator

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password-Generator
+
+Created all the const elements and confirms for all the different character options
+Created alerts if no character sets were selected or if length was too short/long
+Created if statements for each of the characters
+Created a for loop and used math.random to generate each character until selected length of password
---
+++ script.js
@@ -0,0 +1,58 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword () {
+  const passLength = parseInt(prompt("Length?"))
+  
+  if(passLength < 8 || passLength > 128) {
+    alert('Length must be between 8 and 128')
+    return
+  }
+  const lowerChar = 'abc'
+  const upperChar = 'ABC'
+  const numberChar = '123'
+  const specialChar = '!@^'
+  const lowC = confirm('Lower case?')
+  const upC = confirm('Upper case?')
+  const numC = confirm('Number case?')
+  const specC = confirm('Special case?')
+  let passHolder = ''
+  let newPass = ''
+    
+  if(!lowC && !upC && !numC && !specC) {
+    alert('No character set selected')
+    return
+  }
+
+  if(lowC) {
+    passHolder += lowerChar;
+    }
+    if(upC) {
+    passHolder += upperChar;
+    }
+    if(numC) {
+    passHolder += numberChar;
+    }
+    if(specC) {
+    passHolder += specialChar;
+    }
+
+    for(let i = 0; i < passLength; i++) {
+    let randomIndex = Math.floor(Math.random() * passHolder.length)
+    newPass += passHolder[randomIndex]
+    }
+  return newPass
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
